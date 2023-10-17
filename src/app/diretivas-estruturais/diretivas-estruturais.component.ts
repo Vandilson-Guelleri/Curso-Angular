08d637eb512f73b0d2dfbf38d0b6d76475fe2561
Vandilson Guelleri <vandilsonguelleri@gmail.com>
2023-10-17 14:38:56 -0300

    aula 26

---
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-estruturais',
+  templateUrl: './diretivas-estruturais.component.html',
+  styleUrls: ['./diretivas-estruturais.component.scss']
+})
+export class DiretivasEstruturaisComponent {
+
+  public condition: boolean = true;
+
+  ngOnInit():void{
+
+  setInterval(()=>{
+    if(this.condition){
+    this.condition = false;
+    }else{
+      this.condition = true
+    }
+  }, 2000)
+}
+}
